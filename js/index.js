f321425e3c88cd8e5fd705dcc8a4b39d14310d49
Jonatandb <jonatandb@gmail.com>
2024-02-28 19:47:12 -0300

    Add Javascript functionality

---
+++ js/index.js
@@ -0,0 +1,50 @@
+const screen = document.querySelector('.screen');
+const buttons = document.querySelectorAll('.btn')
+
+const errorMsg = 'Error!'
+
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    const pressedButton = button.textContent
+
+    if (button.id === 'clear') {
+      screen.textContent = '0'
+      return
+    }
+
+    if (button.id === 'delete') {
+      if (screen.textContent.length === 1 || screen.textContent.includes(errorMsg)) {
+        screen.textContent = '0'
+      } else {
+        screen.textContent = screen.textContent.slice(0, -1)
+      }
+      return
+    }
+
+    if (button.id === 'equal') {
+      try {
+        screen.textContent = eval(screen.textContent)
+        if (screen.textContent === 'Infinity') {
+          screen.textContent = errorMsg
+        }
+      } catch (error) {
+        screen.textContent = errorMsg
+      }
+      return
+    }
+
+    if (pressedButton === '.') {
+      if (screen.textContent.includes('.')) return
+    }
+
+    if (screen.textContent === '0' || screen.textContent.includes(errorMsg)) {
+      if (pressedButton === '/' || pressedButton === '*' || pressedButton === '-' || pressedButton === '+') {
+        screen.textContent = '0' + pressedButton
+      } else {
+        screen.textContent = pressedButton
+      }
+    } else {
+      screen.textContent += pressedButton
+    }
+  })
+})
